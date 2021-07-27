34c54216a52fd16ca93d5b781ff1b7844c33bf69
unknown <hammambenyounes@gmail.com>
2021-07-27 17:42:17 +0100

    l

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-function handleActive(id, btnClasse) {
-  var divs = document.querySelectorAll(".inside");
-  var btns = document.querySelectorAll(".btn");
+function handleActive(id) {
+  var btns = document.querySelectorAll(".menuBtn");
 
-  for (var i = 0; i < divs.length; i++) {
-    divs[i].classList.remove("active");
-  }
   for (var i = 0; i < btns.length; i++) {
     btns[i].classList.remove("active_input");
   }
-  document.getElementById(id).classList.add("active");
-  document.getElementsByClassName(btnClasse)[0].classList.add("active_input");
+
+  document.getElementById(id).classList.add("active_input");
 }
+
+// Get the modal
+var modal = document.getElementsByClassName("modal");
+
+// Get the button that opens the modal
+var btn = document.getElementsByClassName("modalBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("closeModal");
+
+// When the user clicks the button, open the modal
+btn[0].onclick = function () {
+  modal[0].style.display = "block";
+};
+
+btn[1].onclick = function () {
+  modal[1].style.display = "block";
+};
+// When the user clicks on <span> (x), close the modal
+span[0].onclick = function () {
+  modal[0].style.display = "none";
+};
+
+span[1].onclick = function () {
+  modal[1].style.display = "none";
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal[0]) {
+    modal[0].style.display = "none";
+  }
+  if (event.target == modal[1]) {
+    modal[1].style.display = "none";
+  }
+};
