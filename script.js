97c0d96009b5886877f43885d10b5ccf0b500c7b
hammam <hammambenyounes4@gmail.com>
2022-08-30 20:05:46 +0100

    .

--- script.js
+++ script.js
@@ -25,9 +25,7 @@ btn[0].onclick = function () {
 btn[1].onclick = function () {
   modal[1].style.display = "block";
 };
-btn[2].onclick = function () {
-  modal[2].style.display = "block";
-};
+
 // When the user clicks on <span> (x), close the modal
 span[0].onclick = function () {
   modal[0].style.display = "none";
@@ -36,9 +34,7 @@ span[0].onclick = function () {
 span[1].onclick = function () {
   modal[1].style.display = "none";
 };
-span[2].onclick = function () {
-  modal[2].style.display = "none";
-};
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal[0]) {
